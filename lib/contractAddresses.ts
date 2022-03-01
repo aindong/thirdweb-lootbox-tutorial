db2030c696f4cce321f4d049fca05a64181f9b9e
Alleo Indong <alleo.indong@gmail.com>
2022-03-01 23:09:20 +0800

    give nft pack to users with correct answers

---
+++ lib/contractAddresses.ts
@@ -0,0 +1,2 @@
+export const bundleAddress = "0x7A21ee2d1A27676765d0dED9c9B8c57f65563f66";
+export const packAddress = "0xFb6Ef582D970Ff0DE05C48D91Ca5012313c9db1E";
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import "../styles/globals.css";
+import { ThirdwebProvider } from "@3rdweb/react";
 
 import type { AppProps } from "next/app";
 import Layout from "../components/layout";
 import Head from "next/head";
+
+// Polygon Mumbai chain ID is 80001, see https://chainlist.org
+const supportedChainIds = [80001];
+
+const connectors = {
+  injected: {},
+};
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -10,9 +18,14 @@ export default function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <title>{pageProps.title}</title>
       </Head>
-      <Layout title={pageProps.title}>
-        <Component {...pageProps} />
-      </Layout>
+      <ThirdwebProvider
+        connectors={connectors}
+        supportedChainIds={supportedChainIds}
+      >
+        <Layout title={pageProps.title}>
+          <Component {...pageProps} />
+        </Layout>
+      </ThirdwebProvider>
     </>
   );
 }
